444bf30c8cda2d74207b91cb51957db883e3d695
Timeless <85189523+TimelessOn15Hz@users.noreply.github.com>
2022-11-03 07:06:37 +0200

    Update digitconverter.py

--- digitconverter.py
+++ digitconverter.py
@@ -1,22 +1,66 @@
+"""A simple converter that converts binary, decimal and hexadecimal values, as well as characters in ASCII to their value in decimal and vice versa!"""
+
 import os
 
-
-def binNum():
-    num = int(input("Enter a binary number: "), base=2)
-    print(f"\nBinary (UserInput): {bin(num)}\nDecimal: {int(num)}\nHexadecimal: {hex(num)}")
-    reRun()
+#DEV NOTES:
+#ord(insert char, convert to dec)
+#chr(insert dec, convert to char)
 
 
-def decNum():
-    num = int(input("Enter a decimal number: "), base=10)
-    print(f"\nBinary: {bin(num)}\nDecimal (UserInput): {int(num)}\nHexadecimal: {hex(num)}")
-    reRun()
+def inputBin():
+    try:
+        num = int(input("Enter a binary number: "), base=2)
+        print(f"\nBinary (UserInput): {bin(num)}\nDecimal: {int(num)}\nHexadecimal: {hex(num)}")
+        reRun()
+    except Exception as e:
+        print(e)
+        print("\nMake sure the number you input is BINARY (Base 2)\n")
+        inputBin()
 
 
-def hexNum():
-    num = int(input("Enter a hexadecimal number: "), base=16)
-    print(f"\nBinary: {bin(num)}\nDecimal: {int(num)}\nHexadecimal (UserInput): {hex(num)}")
-    reRun()
+def inputDec():
+    try:
+        num = int(input("Enter a decimal number: "), base=10)
+        print(f"\nBinary: {bin(num)}\nDecimal (UserInput): {int(num)}\nHexadecimal: {hex(num)}")
+        reRun()
+    except Exception as e:
+        print(e)
+        print("\nMake sure the number you input is DECIMAL (Base 10)\n")
+        inputDec()
+
+
+def inputHex():
+    try:
+        num = int(input("Enter a hexadecimal number: "), base=16)
+        print(f"\nBinary: {bin(num)}\nDecimal: {int(num)}\nHexadecimal (UserInput): {hex(num)}")
+        reRun()
+    except Exception as e:
+        print(e)
+        print("\nMake sure the number you input is HEXADECIMAL (Base 16)\n")
+        inputHex()
+
+
+def asciiChar():
+    try:
+        char = str(input("Enter a character (ASCII): "))
+        binNum = bin(ord(char))
+        print(f"\nCharacter (UserInput): {char}\nDecimal: {ord(char)}\nBinary: {binNum}")
+        reRun()
+    except Exception as e:
+        print(e)
+        print("\nMake sure the character you input is an ASCII character.\n")
+        asciiChar()
+
+
+def asciiDec():
+    try:
+        num = int(input("Enter a decimal number: "), base=10)
+        print(f"\nDecimal (UserInput): {num}\nCharacter: {chr(num)}\nBinary: {bin(num)}")
+        reRun()
+    except Exception as e:
+        print(e)
+        print("\nMake sure the number you input is DECIMAL (Base 10)\n")
+        asciiDec()
 
 
 def reRun():
@@ -25,23 +69,41 @@ def reRun():
         Mode()
     elif rerun.lower() == "n":
         print("Exiting program...")
+        exit()
     else:
         print("Exiting program...")
+        exit()
 
 
 def Mode():
-    mode = str(input("Choose mode (bin, dec, hex, chart): "))
+    mode = str(input("Choose mode (bin, dec, hex, ascii, table): "))
 
     if mode.lower() == "bin":
-        binNum()
-    elif mode.lower() == "dec":
-        decNum()
+        inputBin()
+
+    elif mode.lower() == "ascii":
+        def subMode():
+            submode=str(input("Choose submode (char, dec): "))
+            if submode.lower() == "char":
+                asciiChar()
+            elif submode.lower() == "dec":
+                asciiDec()
+            else:
+                print("Invalid submode! Try again!\n")
+                subMode()
+        subMode()
+
     elif mode.lower() == "hex":
-        hexNum()
-    elif mode.lower() == "chart":
+        inputHex()
+
+    elif mode.lower() == "dec":
+        inputDec()
+
+    elif mode.lower() == "table":
         os.startfile('DATACHART.pdf')
-        print("Opening ASCII data chart...")
+        print("\nOpening ASCII data chart...\n")
         reRun()
+
     else:
         print("Invalid mode! Try again!\n")
         Mode()
